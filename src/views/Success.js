b070dcd1120ac521354e755fea5d254779443e9a
Ricky-vitali <89920049+Ricky-vitali@users.noreply.github.com>
2023-08-10 07:11:33 -0300

    Update Success.js

--- src/views/Success.js
+++ src/views/Success.js
@@ -12,15 +12,17 @@ const Success = () => {
     const selectedCarId = queryParams.get("carId");
     const parkingTime = queryParams.get("parkingTime");
     const { currentUser } = useAuth();
+    
+    console.log("Success query params:",queryParams)
 
 
     useEffect(() => {
-        const getOrderStatus = async (parsedId) => {
+        const getOrderStatus = async (collectorId) => {
             try {
                 const YOUR_ACCESS_TOKEN = 'TEST-2039711323530302-072700-102a314cf2e5d98a9a91f5c25c49f643-1102603889'; // Replace this with your MercadoPago access token
-                console.log(`Bearer ${YOUR_ACCESS_TOKEN}`);
+                console.log(`Bearer ${YOUR_ACCESS_TOKEN}`,"Collector id:",collectorId);
                 /* https://www.mercadopago.com.ar/developers/es/reference/payments/_payments_id/get */
-                const response = await fetch(`https://api.mercadopago.com/v1/payments/${parsedId}`, {
+                const response = await fetch(`https://api.mercadopago.com/v1/payments/${collectorId}`, {
                     headers: {
                         Authorization: `Bearer ${YOUR_ACCESS_TOKEN}`,
                     },
@@ -39,7 +41,6 @@ const Success = () => {
         }; 
         getOrderStatus()
     }, [currentUser.uid]);
-
 
     return (
         <>
