04730765672c9a5f6d44822620b6f8e2729d4c75
Ricky-vitali <89920049+Ricky-vitali@users.noreply.github.com>
2023-08-10 06:18:10 -0300

    Update ParkingModal.js

--- src/components/ParkingModal.js
+++ src/components/ParkingModal.js
@@ -42,7 +42,7 @@ const ParkingModal = ( props, onFormSubmit ) => {
                 },
                 external_reference: `${currentUser.uid}`,
            
-                notification_url: "http://localhost:8080/webhook/mercadopago",
+                notification_url: "https://estaciona-chivilcoy.onrender.com/webhook/mercadopago",
             };
 
             const response = await axios.post("https://api.mercadopago.com/checkout/preferences", preferenceData, {
